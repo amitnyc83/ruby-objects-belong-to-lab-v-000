b2c20b533c623f0c321681c47d19236c09e6a16f
Amit Singh <amit_nyc@live.com>
2018-07-02 18:09:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,21 +2,8 @@ class Artist
 
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-
-end
-
-
-class Song
-
-
-  attr_accessor :title, :artist
-
-
-  def initialize(title)
-    @title = title
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
 
 end
